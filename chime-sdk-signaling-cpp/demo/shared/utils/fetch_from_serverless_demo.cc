3e6c102117b56ef10a6ffab1761ac43c2954fe37
Henry Smith <hensmi@amazon.com>
2024-09-06 14:27:54 -0700

    save

--- chime-sdk-signaling-cpp/demo/shared/utils/fetch_from_serverless_demo.cc
+++ chime-sdk-signaling-cpp/demo/shared/utils/fetch_from_serverless_demo.cc
@@ -22,7 +22,7 @@
 
 std::optional<MeetingSessionConfiguration> fetchCredentialsFromServerlessDemo(const std::string& base_url, const std::string& meeting, const std::string& attendee, const std::string& region) {
     // Set up HTTP client
-    httplib::Client cli("https://x95usfs6tl.execute-api.us-east-1.amazonaws.com");
+    httplib::Client cli("https://google.com");
 
     // Create the parameters object
     httplib::Params params;
