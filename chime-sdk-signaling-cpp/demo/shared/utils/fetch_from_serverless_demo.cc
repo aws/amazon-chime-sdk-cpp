c0581cfd956188d4457e5f91175acf853de5440c
Henry Smith <hensmi@amazon.com>
2024-09-06 15:40:55 -0700

    save

--- chime-sdk-signaling-cpp/demo/shared/utils/fetch_from_serverless_demo.cc
+++ chime-sdk-signaling-cpp/demo/shared/utils/fetch_from_serverless_demo.cc
@@ -14,8 +14,6 @@
 #include "signaling/signaling_client_configuration.h"
 #include "signaling/default_signaling_client_factory.h"
 
-#define CPPHTTPLIB_OPENSSL_SUPPORT
-#define OPENSSL_IS_BORINGSSL
 #include <httplib.h>
 
 #include <nlohmann/json.hpp>
